4e7eeadaaa1574288e57ba98ea8dc2b14c836513
Swamy G <swamy@goodguide.com>
2015-07-13 17:19:04 -0700

    Re-arrange order of date and post title

--- wp-content/themes/crawford/content.php
+++ wp-content/themes/crawford/content.php
@@ -3,9 +3,11 @@
 		<div id="featured-image"><?php the_post_thumbnail('featured'); ?></div>		
 	<?php endif; ?>	
 	<div class="col-md-8 col-md-offset-2">			
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<p class="post-date"><?php the_date(); ?> by <?php the_author(); ?> <?php if (is_sticky()) : ?> - Featured<?php endif; ?></p>	
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>	
 			<?php the_title('<h1 id="post-title">', '</h1>'); ?>						
+			<p class="post-date"><?php the_date(); ?> 
+				<span class='author_name'>by <?php the_author(); ?> <?php if (is_sticky()) : ?> - Featured<?php endif; ?></span>
+			</p>
 			<?php the_content(); ?>	
 			<?php $post_tags = get_the_tags(); ?>
 			<?php if ($post_tags) : ?>
@@ -29,8 +31,8 @@
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<?php $title = get_the_title(); ?>
 			<?php if ($title != '') : ?>
-				<p class="post-date"><?php the_time(get_option('date_format')); ?><?php if (is_sticky()) : ?> - Featured<?php endif; ?></p> 
 				<?php the_title('<h3 class="post-title"><a href="' . esc_url( get_permalink()) . '" rel="bookmark">', '</a></h3>'); ?>				
+				<p class="post-date"><?php the_time(get_option('date_format')); ?><?php if (is_sticky()) : ?> - Featured<?php endif; ?></p> 
 			<?php else : ?>
 				<p class="post-date"><a href="<?php echo esc_url( get_permalink()); ?>"><?php the_time(get_option('date_format')); ?><?php if (is_sticky()) : ?> - Featured<?php endif; ?></a></p>
 			<?php endif; ?>			
