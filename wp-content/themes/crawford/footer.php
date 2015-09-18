e8ebc069055f965e9f4c05f250c8d4354ab348db
Swamy G <swamy@goodguide.com>
2015-09-17 19:23:48 -0700

    Add social icons in footer

--- wp-content/themes/crawford/footer.php
+++ wp-content/themes/crawford/footer.php
@@ -2,7 +2,12 @@
 	<?php get_sidebar() ?>
 	<div id="footer-meta" class="container">
 		<div class="row">
-			<p>&copy; <?php echo date("Y"); ?> <a href="<?php echo home_url('/'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a> &bull; <a href="<?php echo esc_url(__('http://www.wpmultiverse.com/themes/crawford/', 'crawford')); ?>" title="Crawford WordPress Theme" target="_blank">Crawford Theme</a></p>
+			<p>&copy; <?php echo date("Y"); ?> <a href="<?php echo home_url('/'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a> &bull; <a href="<?php echo esc_url(__('http://www.wpmultiverse.com/themes/crawford/', 'crawford')); ?>" title="Crawford WordPress Theme" target="_blank">Crawford Theme</a>
+				<a href="http://twitter.com/coachcalledlife" class="social-icon icon-twitter" rel="me" target="_blank">Twitter</a>
+				<a href="http://www.facebook.com/acoachcalledlife" class="social-icon icon-facebook" rel="me" target="_blank">Facebook</a>
+				<a href="https://plus.google.com/u/0/106064074895967401044/posts" class="social-icon icon-google-plus" rel="me author" target="_blank">Google+</a>
+				<a href="<?php echo home_url('/'); ?>feed" class="social-icon icon-rss" rel="me" target="_blank">RSS</a>
+			</p>
 			<nav><?php wp_nav_menu(array('theme_location' => 'footer','container' => false,'depth' => 1,'fallback_cb' => false)); ?></nav>
 		</div>
 	</div>
