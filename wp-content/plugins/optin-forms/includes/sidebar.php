38e8cf3f8da172dbce0909d5d544d28799967f59
Swamy G <swamy@goodguide.com>
2015-10-25 19:43:51 -0700

    Added Optin Forms plugin

---
+++ wp-content/plugins/optin-forms/includes/sidebar.php
@@ -0,0 +1,24 @@
+<?php ?>
+
+                <div class="optinforms-ad-product">
+                    	<h4><a href="https://optinlock.com/?utm_source=OptinForms&utm_medium=link&utm_campaign=sidebar" target="_blank">Get More Subscribers Now!</a></h4>
+                        <p>Boost your conversion rates by locking parts of your posts and pages. Serve your premium content to subscribers only.</p>
+                        <ul>
+                            <li>Create unlimited opt-in forms</li>
+                            <li>Create unlimited popups</li>
+                            <li>Lock your content for non-subscribers</li>
+                            <li>Analyze performance statistics</li>
+                        </ul>
+                    	<div class="optinforms-ad-product-button"><a href="https://optinlock.com/?utm_source=OptinForms&utm_medium=button&utm_campaign=sidebar" target="_blank">Download Optin Lock</a></div>
+                </div><!--optinforms-ad-product-->
+                <div class="optinforms-ad">
+                    <ul>
+                    	<li><a href="https://wordpress.org/support/plugin/optin-forms" target="_blank"><?php echo __('Get support', 'optin-forms'); ?></a></li>
+                        <li><a href="https://wordpress.org/support/view/plugin-reviews/optin-forms" target="_blank"><?php echo __('Rate the plugin on WordPress.org', 'optin-forms'); ?></a></li>
+                        <li><a href="https://translate.wordpress.org/projects/wp-plugins/optin-forms" target="_blank"><?php echo __('Translate Optin Forms into your language', 'optin-forms'); ?></a></li>
+                        <li><a href="https://www.twitter.com/b_ris" target="_blank"><?php echo __('Follow me on Twitter', 'optin-forms'); ?></a></li>
+                        <li><a href="http://brs.io/1PgsNdS" target="_blank"><?php echo __('Try GetResponse for free!', 'optin-forms'); ?></a></li>
+                    </ul>
+                </div><!--optinforms-ad-->
+                
+<?php ?>
