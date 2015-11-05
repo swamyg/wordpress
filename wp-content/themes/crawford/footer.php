9d450a9bde5a96f62695d4c42c63a6aa1aefdd52
Swamy G <swamy@goodguide.com>
2015-11-04 16:01:19 -0800

    Add disclaimer [no story]

--- wp-content/themes/crawford/footer.php
+++ wp-content/themes/crawford/footer.php
@@ -1,6 +1,9 @@
 <footer>
 	<?php get_sidebar() ?>
 	<div id="footer-meta" class="container">
+		<div class="row disclaimer">
+		Swamy G. is not a licensed physician/mental health clinician. The advice and counseling services provided by Swamy are not licensed by the state of California nor are they a substitute for licensed medical treatment.
+	  </div>
 		<div class="row">
 			<p>&copy; <?php echo date("Y"); ?> <a href="<?php echo home_url('/'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a> &bull; <a href="<?php echo esc_url(__('http://www.wpmultiverse.com/themes/crawford/', 'crawford')); ?>" title="Crawford WordPress Theme" target="_blank">Crawford Theme</a>
 				<a href="http://twitter.com/coachcalledlife" class="social-icon icon-twitter" rel="me" target="_blank">Twitter</a>
